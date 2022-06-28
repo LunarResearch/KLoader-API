66ca5df2d0e1fba46fbf0fd2898a13031e251564
LunarResearch <tesla707@mail.ru>
2022-06-28 12:15:48 +0300

    Update KLoaderAPI.h

--- KLoaderAPI.h
+++ KLoaderAPI.h
@@ -68,12 +68,12 @@ class KLoader
 public:
     KLoader();
 
-    NTSTATUS ReferenceModule(_In_ PKLOADER_REFERENCE_MODULE_CONFIG ModuleConfigRef, _Out_ PKLOADER_MODULE_REFERENCE pKModuleRef)
+    NTSTATUS ReferenceModule(_In_ PKLOADER_REFERENCE_MODULE_CONFIG pKModuleConfigRef, _Out_ PKLOADER_MODULE_REFERENCE pKModuleRef)
     {
         KLockHolder m_lock;
         KModule* m_KModule;
 
-        m_lock.m_State = *(uint32_t*)((int8_t*)ModuleConfigRef + 8);
+        m_lock.m_State = *(uint32_t*)((int8_t*)pModuleConfigRef + 8);
 
         auto result = KLoader::ReferenceKModule((PGUID)&m_lock, &m_KModule);
 
